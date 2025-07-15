f926e40658edac24167e2c25fa29fe31d0504efd
Noorpal <noorpal18@gmail.com>
2025-07-14 19:45:02 -0700

    Utils, cors and cookie

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 //require('dotenv').config({path: './.env'})
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
-
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT: ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("ERRR", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
 
 
 
